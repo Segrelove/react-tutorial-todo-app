ce4f917d85a2b68f7856468ba1fd7b558cb58b08
Segrelove <mlesegret@gmail.com>
2020-11-02 15:29:14 +0100

    this tutorial is really slow

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,16 @@
+class Todo extends React.Component {
+  render(){
+    let checked = (this.props.done == "true");
+    
+    return <div className="todo"> 
+            <span>
+              <input type="checkbox" checked={checked} /> 
+              <input type="text" value={this.props.text}/>
+            </span>
+           </div>
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, les petites tchointchoins</h1>,
-  document.getElementById('root'));
+  <Todo text="Todo1" done="false"/>,
+  document.getElementById('root')); 
